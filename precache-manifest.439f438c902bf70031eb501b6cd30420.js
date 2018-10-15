d6f71774523f9ddac420b1f6484a4629995fe0f3
Brian N <brian.noriega3@gmail.com>
2018-10-15 03:45:34 -0400

    Updates

---
+++ precache-manifest.439f438c902bf70031eb501b6cd30420.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6311bf400ad59fe6b6d1",
+    "url": "/wtree-name-game-quiz/static/js/runtime~main.6311bf40.js"
+  },
+  {
+    "revision": "9d7ab75a5a6724abb915",
+    "url": "/wtree-name-game-quiz/static/js/main.9d7ab75a.chunk.js"
+  },
+  {
+    "revision": "63d15fcae6c41b593503",
+    "url": "/wtree-name-game-quiz/static/js/1.63d15fca.chunk.js"
+  },
+  {
+    "revision": "e35168d77eae7e466e62f7d374dcffca",
+    "url": "/wtree-name-game-quiz/index.html"
+  }
+];
